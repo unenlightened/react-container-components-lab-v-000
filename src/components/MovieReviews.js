feb0f591955d170e116e4f01de50cca85d8f056e
Chely Ho <adept_inept@yahoo.com>
2018-09-27 02:28:56 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-const MovieReviews = props => {
-  <ul className='review-list'></ul>
-}
+const MovieReviews = props => {<ul className='review-list'></ul>}
 
 export default MovieReviews
