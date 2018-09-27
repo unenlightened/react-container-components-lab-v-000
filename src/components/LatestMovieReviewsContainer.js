09ce34986c45b23985b0ca757c4990f437fba7d0
Chely Ho <adept_inept@yahoo.com>
2018-09-27 02:36:41 +0000

    automatically backed up by learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,13 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+  state = {
+    reviews: []
+  }
+
+  render() {
+    return <div></div>
+  }
+
+}
