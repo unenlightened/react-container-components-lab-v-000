e570648d9b1d64c58d25b33d6ff977fdfae0bfad
Chely Ho <adept_inept@yahoo.com>
2018-09-27 02:34:55 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MovieReviews = ({ reviews }) => (
   <ul className='review-list'>
-    {reviews.map(review => <li class='review'>boop</li>)}
+    {reviews.map(review => <li className='review'>boop</li>)}
   </ul>
 )
 
